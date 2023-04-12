99aa3f5192943170f70a0b5c98b0d4bcd638af41
Gergely Santa <gergely.santa@bitrise.io>
2023-04-12 21:55:18 +0200

    Fix ci

--- editor/src/engine/graphics/layer/tileLayer.cpp
+++ editor/src/engine/graphics/layer/tileLayer.cpp
@@ -145,8 +145,8 @@ namespace engine
 
     Vec2 TileLayer::getBottomLeftPos(int tileIndex) const
     {
-        int y = tileIndex / m_TileBounds.getWidth();
-        int x = tileIndex % m_TileBounds.getWidth();
+        int y = tileIndex / 1;
+        int x = tileIndex % 1;
         return Vec2(x * m_TileSize + m_Bounds.minX, y * m_TileSize + m_Bounds.minY);
     }
 

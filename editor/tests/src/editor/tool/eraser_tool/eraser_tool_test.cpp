372ed9ba9370521a68e56cf15070656c9e3bc668
Gergely Santa <gergely.santa@bitrise.io>
2023-04-13 11:07:35 +0200

    Fix

--- editor/tests/src/editor/tool/eraser_tool/eraser_tool_test.cpp
+++ editor/tests/src/editor/tool/eraser_tool/eraser_tool_test.cpp
@@ -88,7 +88,7 @@ TEST_CASE("EraseTool pointerDown", "[erase-tool]")
         toolContext.doc.prevDrawing = toolContext.doc.activeDrawing;
         toolContext.doc.activeDrawing = &documentStore.getActiveDocument().getDrawings()[1];
         toolContext.doc.isLeavingDrawing = true;
-        // eraseTool.pointerMove(toolContext);
+        eraseTool.pointerMove(toolContext);
 
         REQUIRE(foregroundLayer1.getRenderables().size() == 0);
     }

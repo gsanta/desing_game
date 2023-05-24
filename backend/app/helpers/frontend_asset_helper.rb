7e78299c47fbd4331db3edf654e0ffd686d7f07f
Gergely Santa <gergely.santa@bitrise.io>
2023-05-24 08:27:11 +0200

    Injecting css

--- backend/app/helpers/frontend_asset_helper.rb
+++ backend/app/helpers/frontend_asset_helper.rb
@@ -1,7 +1,13 @@
 
 module FrontendAssetHelper
-  def javascript_pack_tag(entry)
+  def javascript_asset_tag(entry)
     FrontendAsset.entry(entry).js.map { |js| "<script src=\"#{js}\"></script>" } .join.html_safe
+  rescue NoMethodError => m
+    raise "Invalid entry point #{entry}: #{m}"
+  end
+
+  def css_asset_tag(entry)
+    FrontendAsset.entry(entry).css.map { |url| "<link rel=\"stylesheet\" href=\"#{url}\"></link>" } .join.html_safe
   rescue NoMethodError => m
     raise "Invalid entry point #{entry}: #{m}"
   end

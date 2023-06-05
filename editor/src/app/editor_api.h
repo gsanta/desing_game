3df0625fdd252f7e8841a0e82ead7c229ff1a2c9
Gergely Santa <gergely.santa@bitrise.io>
2023-06-05 22:54:23 +0200

    feat: add circle tool to ui

--- editor/src/app/editor_api.h
+++ editor/src/app/editor_api.h
@@ -3,6 +3,7 @@
 #include "../engine/graphics/layer/tileLayer.h"
 #include "algorithm/flip_horizontal.h"
 #include "document/drawing.h"
+#include "tool/circle_tool/circle_tool.h"
 #include "document/frame_impl.h"
 #include "editor.h"
 

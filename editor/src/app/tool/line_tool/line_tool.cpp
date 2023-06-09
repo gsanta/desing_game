a94de2d0f5ebe1784fc0b4e21a6530fab4edd3e7
Gergely Santa <gergely.santa@bitrise.io>
2023-06-09 22:25:23 +0200

    Drawing slanted lines

--- editor/src/app/tool/line_tool/line_tool.cpp
+++ editor/src/app/tool/line_tool/line_tool.cpp
@@ -4,39 +4,38 @@ namespace spright
 {
 namespace editor
 {
-    LineTool::LineTool(): Tool("line") {
-
+    LineTool::LineTool() : Tool("line")
+    {
     }
 
     void LineTool::pointerDown(const ToolContext &toolContext)
     {
-
     }
 
     void LineTool::pointerMove(const ToolContext &context)
     {
-        if (!context.pointer.isDown) {
+        if (!context.pointer.isDown)
+        {
             return;
         }
 
         TileLayer &foregroundLayer = context.doc.activeDrawing->getForegroundLayer();
         TileLayer &activeLayer = context.doc.activeDrawing->getActiveLayer();
-//        m_Rects.clear();
 
-        std::cout << "clears" << std::endl;
         foregroundLayer.clear();
 
-        Vec2 start = context.pointer.down;
-        Vec2 end = context.pointer.curr;
+        const Vec2 &down = context.pointer.down;
+        const Vec2 &curr = context.pointer.curr;
 
+        Vec2 start = down.x < curr.x ? down : curr;
+        Vec2 end = down.x < curr.x ? curr : down;
 
-//        if ((start - end).length() > 3) {
-//            std::cout << start << " " << end << std::endl;
-//        }
+        float slope = (start.x - end.x) / (start.y - end.y);
 
         for (float x = start.x; x < end.x; x += activeLayer.getTileSize())
         {
-            Vec2Int tilePos = activeLayer.getTilePos(Vec2(x, start.y));
+            float y = start.y + (x - start.x) / slope;
+            Vec2Int tilePos = activeLayer.getTilePos(Vec2(x, y));
 
             brush.paint(foregroundLayer, tilePos, context.editorState->color);
         }
@@ -47,7 +46,8 @@ namespace editor
         TileLayer &foregroundLayer = context.doc.activeDrawing->getForegroundLayer();
         TileLayer &activeLayer = context.doc.activeDrawing->getActiveLayer();
 
-        for (Rect2D* rect : foregroundLayer.getRenderables()) {
+        for (Rect2D *rect : foregroundLayer.getRenderables())
+        {
             activeLayer.add(*rect);
         }
 

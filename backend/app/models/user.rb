c1bb3e8706b58905432d01921c621117eed64634
Gergely Santa <gergely.santa@bitrise.io>
2023-08-06 22:58:08 +0200

    remove omniauth related code

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2, :facebook]
+         :recoverable, :rememberable, :validatable
 
   def self.signin_or_create_from_provider(provider_data)
     where(provider: provider_data[:provider], uid: provider_data[:uid]).first_or_create do |user|
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,8 +6,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {
     sessions: 'users/sessions',
-    registrations: 'users/registrations',
-    omniauth_callbacks: 'users/omniauth_callbacks'
+    registrations: 'users/registrations'
   }
 
   post '/social_auth/callback', to: 'social_auth#authenticate_social_auth_user' # this is the line where we add our routes
